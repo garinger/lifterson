433d2092ba77451ea22952fd8256267bdd39e365
Jackson Garinger <jackson.garinger@gmail.com>
2023-05-13 18:33:00 -0500

    Vercel Postgres

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -1,10 +1,15 @@
+import { sql } from "@vercel/postgres";
+
 export default async function Exercises() {
+  const { rows } = await sql`SELECT * FROM exercises`;
+
   return (
     <>
       <h1 className="text-2xl m-4">Exercises</h1>
       <ul className="m-4">
-        <li>Bench</li>
-        <li>...</li>
+        {rows.map((row) => (
+          <li key={row.id}>{row.exercise_name}</li>
+        ))}
       </ul>
     </>
   );
