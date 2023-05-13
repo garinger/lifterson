3a5dcace400a88fe507112f7f3228423e0ad292a
Jackson Garinger <jackson.garinger@gmail.com>
2023-05-13 18:19:07 -0500

    Removed KV stuff

---
+++ src/app/exercises/page.tsx
@@ -0,0 +1,11 @@
+export default async function Exercises() {
+  return (
+    <>
+      <h1 className="text-2xl m-4">Exercises</h1>
+      <ul className="m-4">
+        <li>Bench</li>
+        <li>...</li>
+      </ul>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import AddExercise from "@/components/AddExercise";
 import Exercise from "@/components/Excercise";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("https://cat-fact.herokuapp.com/facts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <>
-      <h1 className="m-4">Lifterson</h1>
+      <h1 className="text-2xl m-4">Lifterson</h1>
       <AddExercise />
       <Exercise name="Highbar Squat" />
       <Exercise name="Bicep Curls" />
+      <p>{data[0].text}</p>
     </>
   );
 }
