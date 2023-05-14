4ac5644efa4d4d3b0eb4faa753ac203223fa0b5f
Jackson Garinger <jackson.garinger@gmail.com>
2023-05-13 21:16:26 -0500

    so scuffed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,7 @@ export default async function Home() {
       <Link href="/" className="text-2xl m-4">
         Lifterson
       </Link>
-      {/* @ts-expect-error Server Component */}
-      <AddExercise />
+
       <Exercise name="Highbar Squat" />
       <Exercise name="Bicep Curls" />
       <Link href="/exercises" className="text-lg m-4 hover:underline">
